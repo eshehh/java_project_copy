d22c6d24d01406bd0d55e75f175860a37589a1e3
eshehh <qotjsgks1092@naver.com>
2022-12-08 12:49:34 +0900

    저장 221208 4교시

--- java_project/src/ch08_polymorphism_1020/AnimalTest1.java
+++ java_project/src/ch08_polymorphism_1020/AnimalTest1.java
@@ -3,9 +3,24 @@ package ch08_polymorphism_1020;
 import java.util.ArrayList;
 
 class Animal{
+	// 각자 좋아하는 동물 클래스를 만듭니다. ex) Dog
+	// 각 동물들의 공통 기능, move, eat,
+	// 공통 기능 추가 소리나는 기능 추가. sound
+	// Animal 있고, 각 하위 클래스에서 재정의에서 사용을 합니다.
+	// 좋아하는 동물 클래스를 , move, eat, sound 실행해보기
 	public void move()
 	{
 		System.out.println("동물이 움직입니다.");
+	}
+	
+	public void eat()
+	{
+		System.out.println("동물이 먹었다.");
+	}
+	
+	public void sound()
+	{
+		System.out.println("동물이 소리를 냅니다.");
 	}
 }
 
@@ -19,6 +34,16 @@ class Human extends Animal{
 	{
 		System.out.println("사람이 책을 읽습니다. ");
 	}
+	public void eat()
+	{
+		System.out.println("사람이 밥을 먹는다. ");
+	}
+	
+	public void sound()
+	{
+		System.out.println("사람이 목소리를 냅니다.");
+	}
+	
 }
 
 class Tiger extends Animal{
@@ -30,6 +55,16 @@ class Tiger extends Animal{
 	public void hunting() 
 	{
 		System.out.println("호랑이가 사냥을 합니다. ");
+	}
+	
+	public void eat() 
+	{
+		System.out.println("호랑이가 고기를 먹는다. ");
+	}
+	
+	public void sound()
+	{
+		System.out.println("호랑이가 어흥 소리를 냅니다.");
 	}
 }
 
@@ -43,6 +78,16 @@ class Eagle extends Animal{
 	{
 		System.out.print("독수리가 날개를 쭉 펴고 멀리 날아갑니다");
 	}
+	
+	public void eat() 
+	{
+		System.out.print("독수리가 벌레를 먹는다.");
+	}
+	
+	public void sound()
+	{
+		System.out.println("독수리가 까악 소리를 냅니다.");
+	}
 }
 
 class Lsy extends Animal{
@@ -54,6 +99,33 @@ class Lsy extends Animal{
 	public void read() 
 	{
 		System.out.println("이상용 책 봄.");
+	}
+	
+	public void eat() 
+	{
+		System.out.println("이상용 스테이크 먹는다.");
+	}
+	
+	public void sound()
+	{
+		System.out.println("이상용이 무야호 소리를 냅니다.");
+	}
+}
+
+class Dog extends Animal{
+	public void move()
+	{
+		System.out.println("강아지 걸어감.");
+	}
+	
+	public void eat() 
+	{
+		System.out.println("강아지 사료를 먹는다.");
+	}
+	
+	public void sound()
+	{
+		System.out.println("강아지 멍멍 소리를 냅니다.");
 	}
 }
 
@@ -71,12 +143,31 @@ public class AnimalTest1 {
 		  aTest.moveAnimal(new Human());
 		  aTest.moveAnimal(new Tiger());
 		  aTest.moveAnimal(new Eagle());
+		  aTest.moveAnimal(new Lsy());
+//		  
+//		  aTest.eatFood(new Human());
+//		  aTest.eatFood(new Tiger());
+//		  aTest.eatFood(new Eagle());
+//		  aTest.eatFood(new Lsy());
+//		  
+		  aTest.sound(new Human());
+		  aTest.sound(new Tiger());
+		  aTest.sound(new Eagle());
+		  aTest.sound(new Lsy());
 	 }
 
 	 public void moveAnimal(Animal animal) { //매개 변수의 자료형이 상위 클래스
 		  animal.move();                     //재정의 된 메서드 호출
 	 }   
+	 
+	 public void eatFood(Animal animal) {
+		animal.eat();
+	}
 
+	 public void sound(Animal animal) {
+			animal.sound();
+		} 
+	 
 }
 
 

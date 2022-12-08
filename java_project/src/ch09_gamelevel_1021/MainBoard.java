de8ecf920debad4443800fbdf878e5a2756fccbb
eshehh <qotjsgks1092@naver.com>
2022-12-08 16:12:53 +0900

    저장 221208 6교시

--- java_project/src/ch09_gamelevel_1021/MainBoard.java
+++ java_project/src/ch09_gamelevel_1021/MainBoard.java
@@ -26,6 +26,14 @@ public class MainBoard {
 		// 다형성 예졔 ) .. 부모클래스형(타입) level ;
 		// 	level= new BeginnerLevel(); 사용 했습니다. 
 		
+		//추가하기
+		// SuperLevel 다음 단계 : UltraLevel 만들기.
+		// 추상 클래스인 PlayerLevel 상속 받고,
+		// 기능을 구현하는데 . 구현 내용은 각자 알아서 좋게 작성.
+		// 해당 레벨은 4라고 합니다
+		// Player 클래스에 있는 각 메서드 구현
+		
+		
 		
 		Player player = new Player();
 		player.play(1);
@@ -35,6 +43,9 @@ public class MainBoard {
 		SuperLevel sLevel = new SuperLevel();
 		player.upgradeLevel(sLevel);
 		player.play(3);
-		
+		UltraLevel uLevel = new UltraLevel();
+		player.upgradeLevel(uLevel);
+		player.play(4);
+		player.useUltimateSkill(new UltimateSkill("울트라스킬", 5, "전역범위"));
 	}
 }
--- java_project/src/ch09_gamelevel_1021/PlayerLevel.java
+++ java_project/src/ch09_gamelevel_1021/PlayerLevel.java
@@ -7,6 +7,9 @@ public abstract class PlayerLevel {
 	public abstract void jump();
 	public abstract void turn();
 	public abstract void showLevelMessage();
+	 
+		
+	
 	
 	// 템플릿 메서드 , go 메서드 , 매개변수  count 가 들어가고,
 	// run(), 반복문으로 해당 jump , turn.
